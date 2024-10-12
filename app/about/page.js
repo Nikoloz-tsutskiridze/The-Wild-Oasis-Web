82ba7c216698fe3928d464b6b2110cc8fa256c14
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-10-12 13:53:30 +0400

    Setup With Next.js

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
+export default function Page() {
+  return <h1>The Wild Oasis</h1>;
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to paradise.</h1>
+      <Link href="/cabins">explose cabins</Link>
+    </div>
+  );
+}
